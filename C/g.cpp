4ad94e491ab98e7d1e012895cfc0664779de8916
Jimut123 <jimutbahanpal@yahoo.com>
2017-12-29 03:12:16 +0530

    file handling and shits!

--- C/g.cpp
+++ C/g.cpp
@@ -5,10 +5,19 @@
 
 using namespace std;
 
+int fact(int num)
+{
+	if (num == 1) {return 1;}
+	else return num*fact(num-1);
+}
 
 
 int main()
 {
-
+	int n;
+	cout<<"\n Enter a number : "<<endl;
+	cin>>n;
+	int k = fact(n);
+	cout<<"\n The factorial of "<<n<<" is = "<<k<<endl;
 	return 0;
 }
