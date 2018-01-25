b1e4985de3669577cbdc14dbd71cea5464fd5e30
Jimut123 <jimutbahanpal@yahoo.com>
2018-01-25 15:59:52 +0530

    local

--- Syllabus/diff_for.cpp
+++ Syllabus/diff_for.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 int main()
 {
@@ -8,7 +9,7 @@ int main()
 		cout<<"["<<c<<"]";
 	}
 	cout<<endl;
-	return 0;
+	//return EXIT_FAILURE;
 }
 
 
